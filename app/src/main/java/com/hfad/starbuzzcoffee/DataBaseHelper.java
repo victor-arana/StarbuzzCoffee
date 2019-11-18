34b16c024189a868ede2970b8c72d49248d608bb
victor-arana <victor_arana@hotmail.com>
2019-11-18 15:42:56 -0600

    feature, populates the database with data.

--- app/src/main/java/com/hfad/starbuzzcoffee/DataBaseHelper.java
+++ app/src/main/java/com/hfad/starbuzzcoffee/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package com.hfad.starbuzzcoffee;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,7 +10,6 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "starbuzz";
     private static final int DB_VERSION = 1;
 
-
     DataBaseHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
     }
@@ -17,6 +17,24 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        sqLiteDatabase.execSQL("CREATE TABLE DRINK("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DESCRIPTION TEXT, "
+                + "IMAGE_RESOURCE_ID INTEGER);");
+
+        insertDrink(sqLiteDatabase, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(sqLiteDatabase, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+        insertDrink(sqLiteDatabase, "Filter", "Our best drip coffee", R.drawable.filter);
+
+    }
+
+    private void insertDrink(SQLiteDatabase sqLiteDatabase, String name, String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        sqLiteDatabase.insert("DRINK", null, drinkValues);
     }
 
     @Override
