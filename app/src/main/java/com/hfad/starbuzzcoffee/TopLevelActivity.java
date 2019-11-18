1f527fb9c97ce87bc5d59801504e5b88a871bd1a
victor-arana <victor_arana@hotmail.com>
2019-11-18 11:25:04 -0600

    feature, implements onItemClickListener for Drink option.

--- app/src/main/java/com/hfad/starbuzzcoffee/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzzcoffee/TopLevelActivity.java
@@ -2,13 +2,31 @@ package com.hfad.starbuzzcoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class TopLevelActivity extends AppCompatActivity {
+public class TopLevelActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        // Add the listener to the view.
+        ListView options = findViewById(R.id.list_options);
+        options.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        if( position == 0){
+            Toast.makeText(this, "To be implemented", Toast.LENGTH_LONG).show();
+
+        }
     }
 }
