b5c06fb4f90ca1ad936338c4af3ece99701e18de
victor-arana <victor_arana@hotmail.com>
2019-11-18 15:17:47 -0600

    feature, creates database

---
+++ app/src/main/java/com/hfad/starbuzzcoffee/DataBaseHelper.java
@@ -0,0 +1,26 @@
+package com.hfad.starbuzzcoffee;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz";
+    private static final int DB_VERSION = 1;
+
+
+    DataBaseHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
