be1ec1fab889c101dd4f0c7637331fae5d1f16fa
victor-arana <victor_arana@hotmail.com>
2019-11-18 09:56:28 -0600

    refactor, Renames activity_main

--- app/src/main/java/com/hfad/starbuzzcoffee/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzzcoffee/TopLevelActivity.java
@@ -9,6 +9,6 @@ public class TopLevelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_top_level);
     }
 }
