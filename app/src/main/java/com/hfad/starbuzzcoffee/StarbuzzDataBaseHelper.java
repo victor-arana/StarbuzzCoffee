9005f96e9299406328eb9be2bbbee8ca1c04507b
victor.arana <victor.arana@walmart.com>
2019-11-21 11:26:15 -0600

    feature, upgrade drink table for version > 1

--- app/src/main/java/com/hfad/starbuzzcoffee/StarbuzzDataBaseHelper.java
+++ app/src/main/java/com/hfad/starbuzzcoffee/StarbuzzDataBaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "starbuzz";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     StarbuzzDataBaseHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
@@ -16,23 +16,33 @@ public class StarbuzzDataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        // Create table
-        db.execSQL("CREATE TABLE DRINK("
-                                        + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                        + "NAME TEXT, "
-                                        + "DESCRIPTION TEXT, "
-                                        + "IMAGE_RESOURCE_ID INTEGER);");
-
-        // Insert rows
-        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
-        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        updateDatabase(db, 0, DB_VERSION);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDatabase(db, oldVersion, newVersion);
+    }
 
+    private void updateDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion < 1){
+            // Create table
+            db.execSQL("CREATE TABLE DRINK("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "IMAGE_RESOURCE_ID INTEGER);");
+
+            // Insert rows
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        }
+
+        if(oldVersion < 2){
+            // Add extra column
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
     }
 
     private void insertDrink(SQLiteDatabase sqLiteDatabase, String name, String description, int resourceId) {
